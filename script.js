b166ad0c7d01e2e18b6c2eeaf2328e6ff846ddd6
Tony Zhang <zhgtony.pro@hotmail.com>
2022-10-31 02:47:56 +0100

    :construction: cursor

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ menu.onclick = () => {
     menu.classList.toggle('fa-times');
     test.classList.toggle('menu-active');
 }
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute('style', 'top:' + (e.pageY - 10) + "px; left:" + (e.pageX - 10) + "px;")
+})
+document.addEventListener('mousedown', e => {
+    cursor.style.transform = 'scale(.5)'
+
+})
+document.addEventListener('mouseup', e => {
+    cursor.style.transform = 'scale(1)'
+
+})
