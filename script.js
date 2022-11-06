d74193be0bcc5539ebe341f06af24b2641e77a0c
Tony Zhang <zhgtony.pro@hotmail.com>
2022-11-06 22:13:15 +0100

    :lipstick: image hover

--- script.js
+++ script.js
@@ -6,12 +6,23 @@ document.addEventListener('mousemove', e => {
     cursor.setAttribute('style', 'top:' + (e.pageY - 20) + "px; left:" + (e.pageX - 20) + "px;")
 })
 
+
 document.addEventListener('click', () => {
     cursor.classList.add('expand');
 
     setTimeout(() => {
         cursor.classList.remove("expand");
     }, 500);
+})
+
+// --- Image Hover ---
+const imghover = document.querySelector('.hover-image');
+const imghover2 = document.querySelector('.hover-image2');
+const imghover3 = document.querySelector('.hover-image3');
+document.addEventListener('mousemove', e => {
+    imghover.setAttribute('style', 'top:' + (e.pageY) + "px; left:" + (e.pageX) + "px;")
+    imghover2.setAttribute('style', 'top:' + (e.pageY) + "px; left:" + (e.pageX) + "px;")
+    imghover3.setAttribute('style', 'top:' + (e.pageY) + "px; left:" + (e.pageX) + "px;")
 })
 
 // --- Menu hover ---
